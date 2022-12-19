97e38b68a1d92871157a1b90b4646e2051c9409c
groplanda <groplanda@yandex.ru>
2022-12-19 23:58:14 +0300

    fox

--- themes/vue-october/assets/js/components/Popup/Popup.vue
+++ themes/vue-october/assets/js/components/Popup/Popup.vue
@@ -4,7 +4,9 @@
       ._dialog(:class="[className, popupFull]")
         ._content
           button(type="button" @click="onClose" v-if="showClose")._close
-            icon(name="popup-close" component="popup")._close-ico
+            <svg viewBox="0 0 14 14" class="popup__close-ico icon-popup-close ">
+              <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" fill="currentColor"></path>
+            </svg>
           slot
 </template>
 <script>
--- themes/vue-october/assets/js/components/Video/Video.vue
+++ themes/vue-october/assets/js/components/Video/Video.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
-  section.video
+  section.video(:style="{'background-image': `url(${video})`}")
     ._container.container
       ._content
-        ._title Посмотрите видео работы нашего автосервиса!
+        ._title {{title}}
         button._button(type="button" @click="openModal")
-          icon(name="play" component="video")._button-ico
+          <svg viewBox="0 0 22 28" class="video__button-ico icon-play ">
+            <path d="M0 0V28L22 14L0 0Z" fill="currentColor"></path>
+          </svg>
       Popup(className="popup__dialog_video" v-if="popup" @close="closeModal" :fullscreen="fullscreen" :orientation="currentVideo.orientation")
         ._wrap(:class="classVideo")
           video(v-if="!loading" :src="'/storage/app/media/' + currentVideo.video" data-video="main" autoplay @click="stopVideo" @ended="changeVideo('next')")._box
@@ -16,20 +18,30 @@
              span._percent(data-video="time")
 
           button._button.-stop(type="button" @click.stop="playVideo" v-if="stop")
-            icon(name="play" component="video")._button-ico
+            <svg viewBox="0 0 22 28" class="video__button-ico icon-play ">
+                <path d="M0 0V28L22 14L0 0Z" fill="currentColor"></path>
+            </svg>
 
           ._controls
             button._controls-btn.-prev(type="button" @click="changeVideo('prev')")
-              icon(name="next-video" component="video")._controls-ico
+              <svg viewBox="0 0 20 20" class="video__controls-ico icon-next-video ">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM8 14.5L14 10L8 5.5V14.5ZM2 10C2 14.41 5.59 18 10 18C14.41 18 18 14.41 18 10C18 5.59 14.41 2 10 2C5.59 2 2 5.59 2 10Z" fill="currentColor"></path>
+              </svg>
 
             button._controls-btn.-next(type="button" @click="changeVideo('next')")
-              icon(name="next-video" component="video")._controls-ico
+              <svg class="video__controls-ico" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM8 14.5L14 10L8 5.5V14.5ZM2 10C2 14.41 5.59 18 10 18C14.41 18 18 14.41 18 10C18 5.59 14.41 2 10 2C5.59 2 2 5.59 2 10Z" fill="currentColor"/>
+              </svg>
 
             button._controls-btn.-open(type="button" @click="fullscreen = true" v-if="!fullscreen")
-              icon(name="fullscreen_open" component="video")._controls-ico
+              <svg viewBox="0 0 14 14" class="video__controls-ico icon-fullscreen_open ">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M2 5H0V0H5V2H2V5ZM0 9H2V12H5V14H0V9ZM12 12H9V14H14V9H12V12ZM9 2V0H14V5H12V2H9Z" fill="currentColor"></path>
+              </svg>
 
             button._controls-btn.-exit(type="button" @click="fullscreen = false" v-else)
-              icon(name="fullscreen_exit" component="video")._controls-ico
+              <svg viewBox="0 0 14 14" class="video__controls-ico icon-fullscreen_exit ">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M0 3H3V0H5V5H0V3ZM3 11H0V9H5V14H3V11ZM9 14H11V11H14V9H9V14ZM11 0V3H14V5H9V0H11Z" fill="currentColor"></path>
+              </svg>
 
 </template>
 <script>
@@ -41,6 +53,16 @@ export default {
   name: 'Video',
   components: {
     Popup
+  },
+  props: {
+    video: {
+        type: String,
+        default: ''
+    },
+    title: {
+        type: String,
+        default: ''
+    }
   },
   data() {
     return {
